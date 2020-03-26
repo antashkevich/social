f9c5ba5a0f1cc7110e9263f3bb1ef18c024fac5a
Alex Antashkevich <antashkevich@vicman.net>
2020-03-26 15:08:16 +0300

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return <>
+    <div className="preview-block">
+      <img className="preview-block__image" src="https://www.fonedog.com/images/photo-compress/image-compressor-image.jpg" alt='' />
+    </div>
+  </>;
+}
+
+export default Profile;
